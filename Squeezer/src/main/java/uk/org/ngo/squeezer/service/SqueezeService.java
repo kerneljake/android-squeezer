94dde9a4ff9998e6551a4e6fe880c3a61fbf6f07
Jake Angerman <kerneljake@users.noreply.github.com>
2026-09-01 16:51:53 -0400

    debug and staging builds in build.gradle overloaded setState() with system clock in SqueezeService.java

--- Squeezer/src/main/java/uk/org/ngo/squeezer/service/SqueezeService.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/service/SqueezeService.java
@@ -33,6 +33,7 @@ import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.SystemClock;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -398,7 +399,7 @@ public class SqueezeService extends Service {
 
         int playState = isPlaying() ? PlaybackStateCompat.STATE_PLAYING : PlaybackStateCompat.STATE_STOPPED;
         PlaybackStateCompat playbackState = new PlaybackStateCompat.Builder()
-                .setState(playState, player.getPlayerState().getPosition(), isPlaying() ? 1.0f : 0)
+                .setState(playState, player.getPlayerState().getPosition(), isPlaying() ? 1.0f : 0, SystemClock.elapsedRealtime())
                 .setActions(
                         PlaybackStateCompat.ACTION_PLAY |
                                 PlaybackStateCompat.ACTION_PAUSE |
